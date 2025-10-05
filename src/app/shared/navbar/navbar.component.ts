7c6e6ad1983e0c3739a777bd4aee18add5541fdc
dariocon <dariocondepg@gmail.com>
2025-10-05 15:53:43 +0200

    añado desplegable en el icono de perfil

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, CommonModule, NgClass, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../_services/auth.service';
 
@@ -13,13 +13,24 @@ export class NavbarComponent {
   //isAuthenticaded = false;
   constructor(public authService: AuthService){}
 
-
+  isDropdownVisible: boolean = false;
 /*  ngOnInit(): void {
     this.authService.isLogged.subscribe(value => {
       this.isAuthenticaded = value;
     });
   }  */
-  
+  toggleDropdown(): void {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+
+  // Cierra el desplegable cuando el usuario hace clic fuera de él
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+      const clickedInside = (event.target as HTMLElement).closest('.user-profile-container');
+      if (!clickedInside) {
+      this.isDropdownVisible = false;
+      }
+}
   logout(): void {
     this.authService.logout()
   }
